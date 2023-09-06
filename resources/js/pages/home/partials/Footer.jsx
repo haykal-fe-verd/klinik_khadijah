bc749073fd1a458841f48434d59f3d334f13edfe
haykal-fe-verd <haykal@verd.net.id>
2023-09-06 23:33:14 +0700

    add background

--- resources/js/pages/home/partials/Footer.jsx
+++ resources/js/pages/home/partials/Footer.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Link, usePage } from "@inertiajs/react";
-import { Home, Mail, Phone, Printer, SyringeIcon } from "lucide-react";
+import {
+    Home,
+    Instagram,
+    Mail,
+    Phone,
+    Printer,
+    SyringeIcon,
+} from "lucide-react";
 
 function Footer() {
     const { klinik, pelayanan } = usePage().props;
+    console.log("🚀  klinik:", klinik);
 
     const navigation = [
         { name: "Beranda", href: "/" },
@@ -72,12 +80,32 @@ function Footer() {
                             <Phone className="w-5 h-5 mr-5" />
                             {klinik?.telepon}
                         </p>
-                        <p className="flex items-center justify-center md:justify-start">
+                        <p className="flex items-center justify-center mb-4 md:justify-start">
                             <Printer className="w-5 h-5 mr-5" />
                             {klinik?.fax}
                         </p>
+                        <a
+                            href="https://www.instagram.com/klinikkhadijah/"
+                            className="flex items-center justify-center md:justify-start"
+                            target="_blank"
+                        >
+                            <Instagram className="w-5 h-5 mr-5" />
+                            @klinikkhadijah
+                        </a>
                     </div>
                 </div>
+            </div>
+
+            <div id="maps">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.0571127286507!2d95.32639357576737!3d5.558553533622387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3040377645b7a3b1%3A0xfe38e8c0cd282579!2sKlinik%20Khadijah!5e0!3m2!1sen!2sid!4v1694017009128!5m2!1sen!2sid"
+                    width="100%"
+                    height="600"
+                    style={{ border: 0 }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
             </div>
 
             <div className="p-6 text-center bg-gray-100">
